9000048cf6f2beec2b0507e976d91a9b8e89da5b
peaceagent <shahobjon33@gmail.com>
2020-12-17 15:31:25 -0600

    commits

--- normal and arrow functions.js
+++ normal and arrow functions.js
@@ -1,5 +1,38 @@
-//normal functions
 
+//normal functions
+const name = "Max";
+let age = 29;
+const hasHobbies = true;
+age = 30;
+
+function summarizeUser(userName, userAge, userHasHObby) {
+    return ('Name is ' + userName, + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHObby);
+}
 
 
 //arrow functions
+//ES6 came with arrow functions
+//with anonymous function turned into named function with const 
+const summarizeUser = function (userName, userAge, userHasHObby) {
+    return ('Name is ' + userName, + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHObby);
+}
+
+
+//arrow function
+const summarizeUser = (userName, userAge, userHasHObby) => {
+    return ('Name is ' + userName, + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHObby);
+}
+
+//another arrow function
+const add = (a, b) => {
+    return a + b;
+}
+
+//another arrow function
+const add = (a, b) => a + b;
+//another arrow function
+const add = a => a + 1;
+
+
+//arrow function with no paramaters set
+const addRandom = () => 1 + 2;
