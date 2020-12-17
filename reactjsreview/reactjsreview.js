115532a1c342ad8fac055974550514a96c7ff7b1
peaceagent <shahobjon33@gmail.com>
2020-12-17 17:02:13 -0600

    add

---
+++ reactjsreview/reactjsreview.js
@@ -0,0 +1,18 @@
+//reactjsreview
+
+//react.js basics
+//state management & reactivity
+
+
+//React is a javascript library for building User Interfaces(UIs)
+//Browser side javascript library
+
+//react doesnt not runs on servers nor interacts with databases
+
+//react goes with declarative approach: you define the result, not the steps that lead to the result
+
+//imperative approach("normal JS"): You define all the steps to achieve the result (vanilla) and not efficient
+
+// You define components and build your UI with these components.
+
+//Components are UI building blocks
