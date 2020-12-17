a5fbba92605f1943d8bffa204ce51271cba81d0f
peaceagent <shahobjon33@gmail.com>
2020-12-17 16:47:20 -0600

    commit

---
+++ async code, promises, callback.js
@@ -0,0 +1,14 @@
+//Async code, Promises, Callbacks
+
+
+//Async code (executed with defined delay)
+setTimeout(() => {
+    console.log('Timer is done!');
+}, 2000);
+
+
+//Sync code(they are executated right after each other with no delay)
+console.log("hello");
+console.log('Hi');
+
+
