2fcbfa0b87e1acfdcc599feecab0db66b6ade220
peaceagent <shahobjon33@gmail.com>
2020-12-17 16:05:17 -0600

    commit

---
+++ primitives and reference types.js
@@ -0,0 +1,12 @@
+//reference types
+//objects and arrays
+
+//primitives(string, number, boolean, undefined, symbol, BigInt, null)
+
+
+const hobbies = ['Sports', 'Cooking'];
+
+hobbies.push('Programming');
+console.log(hobbies);
+hobbies.pop();
+console.log(hobbies);
