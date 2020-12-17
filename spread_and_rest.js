6d6d7464518630ce2a2d3fd6105cfbe5369ac7db
peaceagent <shahobjon33@gmail.com>
2020-12-17 10:10:18 -0600

    spread and rest operators

---
+++ spread_and_rest.js
@@ -0,0 +1,11 @@
+//spread (all the elements of an array or all the properties of an object )
+const numbers = [1, 2, 3, 4, 5];
+
+const copynumbers = [...numbers];
+
+console.log(numbers);
+console.log(copynumbers);
+
+
+
+//
