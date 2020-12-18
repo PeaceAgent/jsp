12f0d1608696a7a0b132b144568678de48eceeef
peaceagent <shahobjon33@gmail.com>
2020-12-17 19:33:28 -0600

    another commit

--- reactjsreview/reactjsreview.js
+++ reactjsreview/reactjsreview.js
@@ -16,3 +16,14 @@
 // You define components and build your UI with these components.
 
 //Components are UI building blocks
+
+//Short definition
+//React is a JavaScript library for building User Interfaces.
+
+//Longer definition
+//React is a JavaScript library that is only responsible for the view layer of your application.
+//This means it is only responsible for rendering your User Interface (such as the text, text boxes, buttons, etc.) as well as updating the UI whenever it changes.
+
+
+//React is a library and not a framework.
+//The difference between a library and a framework is that a library only helps you in one aspect whereas a framework helps you in many aspects. Let's take an example:
